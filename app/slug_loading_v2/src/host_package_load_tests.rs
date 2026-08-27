f52148534236cf18013709d8de426c84d54d54e2
Ubuntu <wgray@kerrigan-bazzite.lan>
2026-08-27 03:29:42 -0700

    Prove complete configure features freeze

--- app/slug_loading_v2/src/host_package_load_tests.rs
+++ app/slug_loading_v2/src/host_package_load_tests.rs
@@ -14186,6 +14186,240 @@ def link(
                                            feature_configuration.is_enabled("targets_windows")),
     )
 "###;
+const RULES_CC_CONFIGURE_FEATURES_SOURCE: &str = r###"# Copyright 2025 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Helper functions for C++ feature configuration."""
+
+load("//cc:action_names.bzl", "ACTION_NAMES")
+load("//cc/common:semantics.bzl", cc_semantics = "semantics")
+
+ALL_COMPILE_ACTIONS = [
+    ACTION_NAMES.c_compile,
+    ACTION_NAMES.cpp_compile,
+    ACTION_NAMES.cpp_header_parsing,
+    ACTION_NAMES.cpp_module_compile,
+    ACTION_NAMES.cpp_module_codegen,
+    ACTION_NAMES.cpp_module_deps_scanning,
+    ACTION_NAMES.cpp20_module_compile,
+    ACTION_NAMES.cpp20_module_codegen,
+    ACTION_NAMES.assemble,
+    ACTION_NAMES.preprocess_assemble,
+    ACTION_NAMES.clif_match,
+    ACTION_NAMES.linkstamp_compile,
+    ACTION_NAMES.cc_flags_make_variable,
+    ACTION_NAMES.lto_backend,
+    ACTION_NAMES.cpp_header_analysis,
+]
+
+ALL_LINK_ACTIONS = [
+    ACTION_NAMES.lto_index_for_executable,
+    ACTION_NAMES.lto_index_for_dynamic_library,
+    ACTION_NAMES.lto_index_for_nodeps_dynamic_library,
+    ACTION_NAMES.cpp_link_executable,
+    ACTION_NAMES.cpp_link_dynamic_library,
+    ACTION_NAMES.cpp_link_nodeps_dynamic_library,
+]
+
+ALL_ARCHIVE_ACTIONS = [
+    ACTION_NAMES.cpp_link_static_library,
+]
+
+ALL_OTHER_ACTIONS = [
+    ACTION_NAMES.strip,
+]
+
+DEFAULT_ACTION_CONFIGS = ALL_COMPILE_ACTIONS + ALL_LINK_ACTIONS + ALL_ARCHIVE_ACTIONS + ALL_OTHER_ACTIONS
+
+OBJC_ACTIONS = [
+    ACTION_NAMES.objc_compile,
+    ACTION_NAMES.objcpp_compile,
+    ACTION_NAMES.objc_fully_link,
+    ACTION_NAMES.objc_executable,
+]
+
+def _get_coverage_features(cpp_configuration):
+    coverage_features = []
+    coverage_features.append("coverage")
+    if cpp_configuration.use_llvm_coverage_map_format():
+        coverage_features.append("llvm_coverage_map_format")
+    else:
+        coverage_features.append("gcc_coverage_map_format")
+    return coverage_features
+
+def configure_features(
+        *,
+        ctx,
+        cc_toolchain,
+        language = "c++",
+        requested_features = [],
+        unsupported_features = []):
+    """Creates a feature_configuration instance. Requires the cpp configuration fragment.
+
+    Args:
+      ctx: (RuleContext) The rule context.
+      cc_toolchain: (CcToolchainInfo) cc_toolchain for which we configure features.
+      language: ("c++"|"objc"|""objc++") The language to configure for. (default c++)
+      requested_features: (list[str]) List of features to be enabled.
+      unsupported_features: (list[str]) List of features that are unsupported by the current rule.
+
+    Returns:
+      (FeatureConfiguration) The feature configuration.
+    """
+
+    language = (language or "c++").replace("+", "p")
+
+    # TODO(b/236152224): Remove the following when all Starlark objc configure_features have the
+    # chance to migrate to using the language parameter.
+    if "lang_objc" in requested_features:
+        language = "objc"
+
+    if not hasattr(ctx.fragments, "cpp"):
+        fail("cpp configuration fragment is missing")
+
+    cpp_configuration = ctx.fragments.cpp
+
+    if language == "cpp":
+        cc_semantics.validate_layering_check_features(
+            ctx = ctx,
+            cc_toolchain = cc_toolchain,
+            unsupported_features = unsupported_features,
+        )
+
+    all_requested_features_set = set()
+    all_unsupported_features_set = set(unsupported_features)
+
+    if not cc_toolchain._supports_header_parsing:
+        # TODO(b/159096411): Remove once supports_header_parsing has been removed from the
+        # cc_toolchain rule.
+        all_unsupported_features_set.add("parse_headers")
+
+    if (language != "objc" and
+        language != "objcpp" and
+        cc_toolchain._cc_info.compilation_context._module_map == None):
+        all_unsupported_features_set.add("module_maps")
+
+    if cpp_configuration.force_pic():
+        if "supports_pic" in all_unsupported_features_set:
+            fail("PIC compilation is requested but the toolchain does not support it " +
+                 "(feature named 'supports_pic' is not enabled)")
+        all_requested_features_set.add("supports_pic")
+
+    if cpp_configuration.apple_generate_dsym:
+        all_requested_features_set.add("generate_dsym_file")
+    else:
+        all_requested_features_set.add("no_generate_debug_symbols")
+
+    if language == "objc" or language == "objcpp":
+        all_requested_features_set.add("lang_objc")
+        if cpp_configuration.objc_generate_linkmap:
+            all_requested_features_set.add("generate_linkmap")
+        if cpp_configuration.objc_should_strip_binary:
+            all_requested_features_set.add("dead_strip")
+
+    all_features = [cpp_configuration.compilation_mode()]
+    all_features.extend(DEFAULT_ACTION_CONFIGS)
+    all_features.extend(requested_features)
+    all_features.extend(cc_toolchain._toolchain_features.default_features_and_action_configs())
+
+    if language == "objc" or language == "objcpp":
+        all_features.extend(OBJC_ACTIONS)
+
+    if not cpp_configuration._dont_enable_host_nonhost:
+        if cc_toolchain._is_tool_configuration:
+            all_features.append("host")
+        else:
+            all_features.append("nonhost")
+
+    if ctx.configuration.coverage_enabled:
+        all_features.extend(_get_coverage_features(cpp_configuration))
+
+    if "fdo_instrument" not in all_unsupported_features_set:
+        if cpp_configuration.fdo_instrument() != None:
+            all_features.append("fdo_instrument")
+        elif cpp_configuration.cs_fdo_instrument() != None:
+            all_features.append("cs_fdo_instrument")
+
+    fdo_context = cc_toolchain._fdo_context
+    branch_fdo_provider = getattr(fdo_context, "branch_fdo_profile", None)
+
+    enable_propeller_optimize = (
+        getattr(fdo_context, "propeller_optimize_info", None) != None and
+        (fdo_context.propeller_optimize_info.cc_profile != None or
+         fdo_context.propeller_optimize_info.ld_profile != None)
+    )
+
+    if branch_fdo_provider != None and cpp_configuration.compilation_mode() == "opt":
+        if ((branch_fdo_provider.branch_fdo_mode == "llvm_fdo" or
+             branch_fdo_provider.branch_fdo_mode == "llvm_cs_fdo") and
+            "fdo_optimize" not in all_unsupported_features_set):
+            all_features.append("fdo_optimize")
+            if "memprof_optimize" not in all_unsupported_features_set:
+                all_features.append("enable_fdo_memprof_optimize")
+            if "thin_lto" not in all_unsupported_features_set:
+                all_features.append("enable_fdo_thinlto")
+            if ("split_functions" not in all_unsupported_features_set and
+                not enable_propeller_optimize):
+                all_features.append("enable_fdo_split_functions")
+
+        if branch_fdo_provider.branch_fdo_mode == "llvm_cs_fdo":
+            all_features.append("cs_fdo_optimize")
+
+        if branch_fdo_provider.branch_fdo_mode == "auto_fdo":
+            all_features.append("autofdo")
+            if "memprof_optimize" not in all_unsupported_features_set:
+                all_features.append("enable_autofdo_memprof_optimize")
+            if "thin_lto" not in all_unsupported_features_set:
+                all_features.append("enable_afdo_thinlto")
+            if "fsafdo" not in all_unsupported_features_set:
+                all_features.append("enable_fsafdo")
+                if "split_functions" not in all_unsupported_features_set:
+                    all_features.append("enable_fdo_split_functions")
+
+        if branch_fdo_provider.branch_fdo_mode == "xbinary_fdo":
+            all_features.append("xbinaryfdo")
+            if "thin_lto" not in all_unsupported_features_set:
+                all_features.append("enable_xbinaryfdo_thinlto")
+
+    if cpp_configuration._fdo_prefetch_hints_label != None:
+        all_requested_features_set.add("fdo_prefetch_hints")
+
+    if enable_propeller_optimize:
+        all_requested_features_set.add("propeller_optimize")
+
+    for feature in all_features:
+        if feature not in all_unsupported_features_set:
+            all_requested_features_set.add(feature)
+
+    feature_configuration = cc_toolchain._toolchain_features.configure_features(
+        requested_features = list(all_requested_features_set),
+    )
+
+    for feature in all_unsupported_features_set:
+        if feature_configuration.is_enabled(feature):
+            fail(("The C++ toolchain '{}' unconditionally implies feature '{}', which is unsupported " +
+                  "by this rule. This is most likely a misconfiguration in the C++ toolchain.")
+                .format(cc_toolchain._toolchain_label, feature))
+
+    if (cpp_configuration.force_pic() and
+        not feature_configuration.is_enabled("pic") and
+        not feature_configuration.is_enabled("supports_pic")):
+        fail("PIC compilation is requested but the toolchain does not support it " +
+             "(feature named 'supports_pic' is not enabled)")
+
+    return feature_configuration
+"###;
+
 const RULES_CC_TOOLCHAIN_CONFIG_INFO_SOURCE: &str = r###"# Copyright 2025 The Bazel Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -23256,6 +23490,178 @@ fn exact_rules_cc_toolchain_config_info_freezes_complete_recursive_producer() {
     )
     .unwrap();
     assert_rules_cc_toolchain_config_info_interfaces(&module, &children);
+}
+
+fn assert_rules_cc_action_name_list(
+    module: &FrozenModule,
+    action_names: &StructRef<'_>,
+    binding: &str,
+    fields: &[&str],
+) {
+    let list = FrozenListRef::from_value(module.get(binding).unwrap().value()).unwrap();
+    assert_eq!(list.len(), fields.len(), "{binding}");
+    for (actual, field) in list.iter().zip(fields) {
+        let expected = action_names
+            .iter()
+            .find_map(|(name, value)| (name.as_str() == *field).then_some(value))
+            .unwrap();
+        assert_eq!(
+            actual.to_value().unpack_str(),
+            expected.unpack_str(),
+            "{binding}/{field}"
+        );
+    }
+}
+
+fn assert_rules_cc_configure_features_interfaces(
+    module: &FrozenModule,
+    action_names: &FrozenModule,
+    semantics: &FrozenModule,
+) {
+    assert!(
+        module
+            .get("ACTION_NAMES")
+            .unwrap()
+            .value()
+            .ptr_eq(action_names.get("ACTION_NAMES").unwrap().value())
+    );
+    assert!(
+        module
+            .get("cc_semantics")
+            .unwrap()
+            .value()
+            .ptr_eq(semantics.get("semantics").unwrap().value())
+    );
+    let names_value = action_names.get("ACTION_NAMES").unwrap();
+    let names = StructRef::from_value(names_value.value()).unwrap();
+    let compile = [
+        "c_compile",
+        "cpp_compile",
+        "cpp_header_parsing",
+        "cpp_module_compile",
+        "cpp_module_codegen",
+        "cpp_module_deps_scanning",
+        "cpp20_module_compile",
+        "cpp20_module_codegen",
+        "assemble",
+        "preprocess_assemble",
+        "clif_match",
+        "linkstamp_compile",
+        "cc_flags_make_variable",
+        "lto_backend",
+        "cpp_header_analysis",
+    ];
+    let link = [
+        "lto_index_for_executable",
+        "lto_index_for_dynamic_library",
+        "lto_index_for_nodeps_dynamic_library",
+        "cpp_link_executable",
+        "cpp_link_dynamic_library",
+        "cpp_link_nodeps_dynamic_library",
+    ];
+    let archive = ["cpp_link_static_library"];
+    let other = ["strip"];
+    let objc = [
+        "objc_compile",
+        "objcpp_compile",
+        "objc_fully_link",
+        "objc_executable",
+    ];
+    for (binding, fields) in [
+        ("ALL_COMPILE_ACTIONS", compile.as_slice()),
+        ("ALL_LINK_ACTIONS", link.as_slice()),
+        ("ALL_ARCHIVE_ACTIONS", archive.as_slice()),
+        ("ALL_OTHER_ACTIONS", other.as_slice()),
+        ("OBJC_ACTIONS", objc.as_slice()),
+    ] {
+        assert_rules_cc_action_name_list(module, &names, binding, fields);
+    }
+    let defaults = compile
+        .into_iter()
+        .chain(link)
+        .chain(archive)
+        .chain(other)
+        .collect::<Vec<_>>();
+    assert_rules_cc_action_name_list(module, &names, "DEFAULT_ACTION_CONFIGS", &defaults);
+    assert_eq!(
+        module.get("configure_features").unwrap().value().get_type(),
+        "function"
+    );
+    assert_eq!(
+        module
+            .get_any_visibility("_get_coverage_features")
+            .unwrap()
+            .0
+            .value()
+            .get_type(),
+        "function"
+    );
+    assert!(module.get("_get_coverage_features").is_err());
+    let mut public = module.names().map(|name| name.as_str()).collect::<Vec<_>>();
+    public.sort_unstable();
+    let mut expected = "ACTION_NAMES ALL_ARCHIVE_ACTIONS ALL_COMPILE_ACTIONS ALL_LINK_ACTIONS ALL_OTHER_ACTIONS DEFAULT_ACTION_CONFIGS OBJC_ACTIONS cc_semantics configure_features".split_whitespace().collect::<Vec<_>>();
+    expected.sort_unstable();
+    assert_eq!(public, expected);
+    let mut all = module
+        .names_any_visibility()
+        .map(|name| name.as_str())
+        .collect::<Vec<_>>();
+    all.sort_unstable();
+    expected.push("_get_coverage_features");
+    expected.sort_unstable();
+    assert_eq!(all, expected);
+}
+
+#[test]
+fn exact_rules_cc_configure_features_freezes_complete_recursive_producer() {
+    assert_eq!(RULES_CC_CONFIGURE_FEATURES_SOURCE.lines().count(), 232);
+    assert_eq!(
+        format!(
+            "{:x}",
+            Sha256::digest(RULES_CC_CONFIGURE_FEATURES_SOURCE.as_bytes())
+        ),
+        "d950aa9acda68b999c452178f8ccf49860eac910a8c28551c547c3725198b977"
+    );
+    let action_owner = BzlModuleIdentity {
+        label: CanonicalLabel::parse("@@rules_cc+//cc:action_names.bzl").unwrap(),
+        workspace_path: PathBuf::from("/rules_cc/cc/action_names.bzl"),
+        repository_mapping: Arc::from([]),
+    };
+    let semantics_owner = BzlModuleIdentity {
+        label: CanonicalLabel::parse("@@rules_cc+//cc/common:semantics.bzl").unwrap(),
+        workspace_path: PathBuf::from("/rules_cc/cc/common/semantics.bzl"),
+        repository_mapping: Arc::from([(
+            ApparentRepoName::new("platforms").unwrap(),
+            CanonicalRepoName::new("platforms+").unwrap(),
+        )]),
+    };
+    let action_names =
+        eval_bzl_with_identity(RULES_CC_ACTION_NAMES_SOURCE, action_owner.clone()).unwrap();
+    let semantics =
+        eval_bzl_with_identity(RULES_CC_SEMANTICS_SOURCE, semantics_owner.clone()).unwrap();
+    let module = eval_bzl_with_loaded_children(
+        RULES_CC_CONFIGURE_FEATURES_SOURCE,
+        BzlModuleIdentity {
+            label: CanonicalLabel::parse(
+                "@@rules_cc+//cc/private/toolchain_config:configure_features.bzl",
+            )
+            .unwrap(),
+            workspace_path: PathBuf::from(
+                "/rules_cc/cc/private/toolchain_config/configure_features.bzl",
+            ),
+            repository_mapping: Arc::from([]),
+        },
+        &[
+            ("//cc:action_names.bzl", action_owner, action_names.dupe()),
+            (
+                "//cc/common:semantics.bzl",
+                semantics_owner,
+                semantics.dupe(),
+            ),
+        ],
+    )
+    .unwrap();
+    assert_rules_cc_configure_features_interfaces(&module, &action_names, &semantics);
 }
 
 #[test]
