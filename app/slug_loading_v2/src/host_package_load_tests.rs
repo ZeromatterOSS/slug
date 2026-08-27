01920f59415a22aa37dcb61fdb71325c53ebfbec
Ubuntu <wgray@kerrigan-bazzite.lan>
2026-08-27 04:21:11 -0700

    Prove complete rules_rust LTO freeze

--- app/slug_loading_v2/src/host_package_load_tests.rs
+++ app/slug_loading_v2/src/host_package_load_tests.rs
@@ -4158,6 +4158,127 @@ load("@cc_compatibility_proxy//:symbols.bzl", _CcInfo = "CcInfo")
 
 CcInfo = _CcInfo
 "###;
+const RULES_RUST_LTO_SOURCE: &str = r###""""A module defining Rust link time optimization (lto) rules"""
+
+load("//rust/private:utils.bzl", "is_exec_configuration")
+
+_LTO_MODES = [
+    # Do nothing, let the user manually handle LTO.
+    "manual",
+    # Default. No mode has been explicitly set, rustc will do "thin local" LTO
+    # between the codegen units of a single crate.
+    "unspecified",
+    # LTO has been explicitly turned "off".
+    "off",
+    # Perform "thin" LTO. This is similar to "fat" but takes significantly less
+    # time to run, but provides similar performance improvements.
+    #
+    # See: <http://blog.llvm.org/2016/06/thinlto-scalable-and-incremental-lto.html>
+    "thin",
+    # Perform "fat"/full LTO.
+    "fat",
+]
+
+RustLtoInfo = provider(
+    doc = "A provider describing the link time optimization setting.",
+    fields = {"mode": "string: The LTO mode specified via a build setting."},
+)
+
+def _rust_lto_flag_impl(ctx):
+    value = ctx.build_setting_value
+
+    if value not in _LTO_MODES:
+        msg = "{NAME} build setting allowed to take values [{VALUES}], but was set to: {ACTUAL}".format(
+            NAME = ctx.label,
+            VALUES = ", ".join(["'{}'".format(m) for m in _LTO_MODES]),
+            ACTUAL = value,
+        )
+        fail(msg)
+
+    return RustLtoInfo(mode = value)
+
+rust_lto_flag = rule(
+    doc = "A build setting which specifies the link time optimization mode used when building Rust code. Allowed values are: ".format(_LTO_MODES),
+    implementation = _rust_lto_flag_impl,
+    build_setting = config.string(flag = True),
+)
+
+def _determine_lto_object_format(ctx, toolchain, crate_info):
+    """Determines if we should run LTO and what bitcode should get included in a built artifact.
+
+    Args:
+        ctx (ctx): The calling rule's context object.
+        toolchain (rust_toolchain): The current target's `rust_toolchain`.
+        crate_info (CrateInfo): The CrateInfo provider of the target crate.
+
+    Returns:
+        string: Returns one of only_object, only_bitcode, object_and_bitcode.
+    """
+
+    # Even if LTO is enabled don't use it for actions being built in the exec
+    # configuration, e.g. build scripts and proc-macros. This mimics Cargo.
+    if is_exec_configuration(ctx):
+        return "only_object"
+
+    mode = toolchain.lto.mode
+
+    if mode in ["off", "unspecified"]:
+        return "only_object"
+
+    perform_linking = crate_info.type in ["bin", "staticlib", "cdylib"]
+    is_dynamic = crate_info.type in ["dylib", "cdylib", "proc-macro"]
+    needs_object = perform_linking or is_dynamic
+
+    # At this point we know LTO is enabled, otherwise we would have returned above.
+
+    if not needs_object:
+        # If we're building an 'rlib' and LTO is enabled, then we can skip
+        # generating object files entirely.
+        return "only_bitcode"
+    elif crate_info.type in ["dylib", "proc-macro"]:
+        # If we're a dylib and we're running LTO, then only emit object code
+        # because 'rustc' doesn't currently support LTO with dylibs.
+        # proc-macros do not benefit from LTO, and cannot be dynamically linked with LTO.
+        return "only_object"
+    else:
+        return "object_and_bitcode"
+
+def construct_lto_arguments(ctx, toolchain, crate_info):
+    """Returns a list of 'rustc' flags to configure link time optimization.
+
+    Args:
+        ctx (ctx): The calling rule's context object.
+        toolchain (rust_toolchain): The current target's `rust_toolchain`.
+        crate_info (CrateInfo): The CrateInfo provider of the target crate.
+
+    Returns:
+        list: A list of strings that are valid flags for 'rustc'.
+    """
+    mode = toolchain.lto.mode
+
+    # The user is handling LTO on their own, don't add any arguments.
+    if mode == "manual":
+        return []
+
+    format = _determine_lto_object_format(ctx, toolchain, crate_info)
+    args = []
+
+    # proc-macros do not benefit from LTO, and cannot be dynamically linked with LTO.
+    if mode in ["thin", "fat", "off"] and not is_exec_configuration(ctx) and crate_info.type != "proc-macro":
+        args.append("-Clto=%s" % mode)
+
+    if format == "object_and_bitcode":
+        # Embedding LLVM bitcode in object files is `rustc's` default.
+        pass
+    elif format == "only_object":
+        args.append("-Cembed-bitcode=no")
+    elif format == "only_bitcode":
+        args.append("-Clinker-plugin-lto")
+    else:
+        fail("unrecognized LTO object format %s" % format)
+
+    return args
+"###;
 const RULES_RUST_UTILS_SOURCE: &str = r###"# Copyright 2015 The Bazel Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -27128,6 +27249,120 @@ fn exact_rules_rust_providers_freeze_complete_family() {
         .collect::<Vec<_>>();
     all.sort_unstable();
     assert_eq!(all, expected);
+}
+
+#[test]
+fn exact_rules_rust_lto_freezes_complete_recursive_producer() {
+    assert_eq!(RULES_RUST_LTO_SOURCE.lines().count(), 120);
+    assert_eq!(
+        format!("{:x}", Sha256::digest(RULES_RUST_LTO_SOURCE.as_bytes())),
+        "9907a2411a51f0acd36131d7a695ac4fb244c4c5482c3f8bb6a5d0194abef924"
+    );
+    let utils_children = complete_rules_rust_utils_children();
+    let utils_owner = compile_owner(
+        "@@rules_rust+//rust/private:utils.bzl",
+        "/rules_rust/rust/private/utils.bzl",
+        &[("bazel_skylib", "bazel_skylib+"), ("rules_cc", "rules_cc+")],
+    );
+    let utils = eval_bzl_with_loaded_children(
+        RULES_RUST_UTILS_SOURCE,
+        utils_owner.clone(),
+        &utils_children,
+    )
+    .unwrap();
+    let module = eval_bzl_with_loaded_children(
+        RULES_RUST_LTO_SOURCE,
+        compile_owner(
+            "@@rules_rust+//rust/private:lto.bzl",
+            "/rules_rust/rust/private/lto.bzl",
+            &[],
+        ),
+        &[("//rust/private:utils.bzl", utils_owner, utils.dupe())],
+    )
+    .unwrap();
+    assert!(
+        module
+            .get("is_exec_configuration")
+            .unwrap()
+            .value()
+            .ptr_eq(utils.get("is_exec_configuration").unwrap().value())
+    );
+    let modes = module.get_any_visibility("_LTO_MODES").unwrap().0;
+    assert_eq!(
+        FrozenListRef::from_value(modes.value())
+            .unwrap()
+            .iter()
+            .map(|value| value.to_value().unpack_str().unwrap())
+            .collect::<Vec<_>>(),
+        ["manual", "unspecified", "off", "thin", "fat"]
+    );
+    assert!(module.get("_LTO_MODES").is_err());
+    let provider = module.get("RustLtoInfo").unwrap();
+    let provider = FrozenUserProviderCallable::from_value(provider.value()).unwrap();
+    assert_eq!(
+        provider.id().source_label(),
+        "@@rules_rust+//rust/private:lto.bzl"
+    );
+    assert_eq!(provider.id().exported_name(), "RustLtoInfo");
+    let rule = module
+        .get("rust_lto_flag")
+        .unwrap()
+        .downcast::<FrozenRuleDefinition>()
+        .unwrap();
+    assert_eq!(rule.capability().rule_class, "rust_lto_flag");
+    assert_eq!(
+        rule.build_setting_kind,
+        Some(BuildSettingKind::String {
+            flag: true,
+            allow_multiple: false,
+        })
+    );
+    for name in [
+        "construct_lto_arguments",
+        "_rust_lto_flag_impl",
+        "_determine_lto_object_format",
+    ] {
+        assert_eq!(
+            module
+                .get_any_visibility(name)
+                .unwrap()
+                .0
+                .value()
+                .get_type(),
+            "function"
+        );
+    }
+    for name in ["_rust_lto_flag_impl", "_determine_lto_object_format"] {
+        assert!(module.get(name).is_err());
+    }
+    let mut public = module.names().map(|name| name.as_str()).collect::<Vec<_>>();
+    public.sort_unstable();
+    assert_eq!(
+        public,
+        [
+            "RustLtoInfo",
+            "construct_lto_arguments",
+            "is_exec_configuration",
+            "rust_lto_flag"
+        ]
+    );
+    let mut all = module
+        .names_any_visibility()
+        .map(|name| name.as_str())
+        .collect::<Vec<_>>();
+    all.sort_unstable();
+    assert_eq!(
+        all,
+        [
+            "RustLtoInfo",
+            "_LTO_MODES",
+            "_determine_lto_object_format",
+            "_rust_lto_flag_impl",
+            "construct_lto_arguments",
+            "is_exec_configuration",
+            "rust_lto_flag",
+        ]
+    );
 }
 
 fn assert_rules_rust_utils_imports(
