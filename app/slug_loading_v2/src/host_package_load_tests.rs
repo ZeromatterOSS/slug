c4e49424c5f03ec1368d5141c6b81bd23a64de88
Ubuntu <wgray@kerrigan-bazzite.lan>
2026-08-26 19:01:49 -0700

    Prove complete rules_cc private paths freeze

--- app/slug_loading_v2/src/host_package_load_tests.rs
+++ app/slug_loading_v2/src/host_package_load_tests.rs
@@ -3977,6 +3977,46 @@ const RULES_CC_INTERNAL_SOURCE: &str = r###"# Copyright 2025 The Bazel Authors. 
 # buildifier: disable=native-cc-common
 cc_internal = cc_common.internal_DO_NOT_USE() if hasattr(cc_common, "internal_DO_NOT_USE") else struct()
 "###;
+const RULES_CC_PRIVATE_PATHS_SOURCE: &str = r###"# Copyright 2025 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Paths utility functions."""
+
+def is_path_absolute(path):
+    """Checks whether a path is absolute or not.
+
+    Note that is does not take the execution platform into account.
+
+    This was implemented to replace skylib's paths.is_absolute, which
+    only checks for the presence of a colon character in the second position
+    of the paths
+
+    Args:
+      path: A path (as a string).
+
+    Returns:
+      `True` if `path` is an absolute path.
+    """
+
+    if path.startswith("/"):
+        return True
+
+    # Check for DOS-style absolute paths for Windows
+    return len(path) >= 3 and \
+           path[0].isalpha() and \
+           path[1] == ":" and \
+           path[2] in ("/", "\\")
+"###;
 const RULES_CC_OBJC_INFO_SOURCE: &str = r###"# Copyright 2024 The Bazel Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -5556,6 +5596,32 @@ fn exact_rules_cc_internal_complete_source_freezes_selected_private_bridge() {
             .unwrap_err()
             .contains("cc_common")
     );
+}
+
+#[test]
+fn exact_rules_cc_private_paths_complete_source_freezes_lazy_export() {
+    assert_eq!(RULES_CC_PRIVATE_PATHS_SOURCE.lines().count(), 39);
+    assert_eq!(
+        format!(
+            "{:x}",
+            Sha256::digest(RULES_CC_PRIVATE_PATHS_SOURCE.as_bytes())
+        ),
+        "c982ac685f0bfbd32602d82d1c37f3bf50a2714ca6a13bfd3c08d4e5cc8b8872"
+    );
+    let module = eval_bzl_with_identity(
+        RULES_CC_PRIVATE_PATHS_SOURCE,
+        BzlModuleIdentity {
+            label: CanonicalLabel::parse("@@rules_cc+//cc/private:paths.bzl").unwrap(),
+            workspace_path: PathBuf::from("/rules_cc/cc/private/paths.bzl"),
+            repository_mapping: Arc::from([]),
+        },
+    )
+    .unwrap();
+    assert_eq!(
+        module.get("is_path_absolute").unwrap().value().get_type(),
+        "function"
+    );
+    assert!(module.get("_is_path_absolute").is_err());
 }
 
 #[test]
